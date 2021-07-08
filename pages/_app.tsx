f1ed5eac73fddb796fe896dd1b2b690576092b7b
re7za <syd.id7@gmail.com>
2021-07-08 21:26:58 +0430

    Add font to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import style from "../assets/styles/app.module.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={style.root}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;

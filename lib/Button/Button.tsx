679a1d4c305e70201c29994afe6ae78eea8a45f9
re7za <syd.id7@gmail.com>
2021-07-09 21:20:38 +0430

    Add desabled option to Button

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -4,7 +4,12 @@ import style from "../../assets/styles/lib/button/style.module.scss";
 
 const Button = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button {...props} className={`${style.button} ${props.className}`}>
+    <button
+      {...props}
+      className={`${style.button} ${props.className} ${
+        props.disabled ? style.disabled : ""
+      }`}
+    >
       {props.children}
     </button>
   );

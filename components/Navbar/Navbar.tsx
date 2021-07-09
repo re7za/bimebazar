97cb1f5dd3d2bcdd5c138853bace57d9ebbd60c6
re7za <syd.id7@gmail.com>
2021-07-09 18:05:18 +0430

    Add Login Modal to Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,16 +4,29 @@ import style from "../../assets/styles/components/navbar/style.module.scss";
 
 // Misc
 import Button from "../../lib/Button";
+import Login from "../Login";
+import useModal from "../../lib/useModal";
 
 const Navbar = () => {
+  const loginModal = useModal({
+    title: "بیمه بازار",
+  });
+
   return (
-    <div className={style.navbar}>
-      <div>بیمه بازار</div>
-      <div className={style.buttons}>
-        <Button className={style.participationBtn}>شرکت در مسابقه</Button>
-        <Button className={style.login}>ورود / ثبت نام</Button>
+    <>
+      <div className={style.navbar}>
+        <div>بیمه بازار</div>
+        <div className={style.buttons}>
+          <Button className={style.participationBtn}>شرکت در مسابقه</Button>
+          <Button className={style.login} onClick={loginModal.open}>
+            ورود / ثبت نام
+          </Button>
+        </div>
       </div>
-    </div>
+      <loginModal.Modal>
+        <Login />
+      </loginModal.Modal>
+    </>
   );
 };
 
